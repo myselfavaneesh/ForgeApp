f0636726e18d553e0c3ee314e4af979048f7a8f0
myselfavaneesh <avaneeshjaiswal2k25@gmail.com>
2026-01-20 17:01:47 +0530

    changes

--- babel.config.js
+++ babel.config.js
@@ -4,6 +4,9 @@ module.exports = function (api) {
     presets: ['babel-preset-expo'],
     plugins: [
       ['@babel/plugin-proposal-decorators', { legacy: true }],
+      ['@babel/plugin-proposal-class-properties', { loose: true }],
+      'react-native-reanimated/plugin',
+      ["@lovesworking/watermelondb-expo-plugin-sdk-52-plus"],
       'react-native-reanimated/plugin',
     ],
   }
